6a56db4ae5254a2238f1ad30ddfa2b87f8a77276
Bogdan Tkach <tkachbogdan0205@gmail.com>
2022-04-29 23:16:41 +0300

    Tailwind settings

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      'lg': {'max':'998px'},
+      'md': {'max':'786px'},
+      'sm': {'max':'460px'},
+    },
+    extend: {},
+  },
+  plugins: [],
+}
